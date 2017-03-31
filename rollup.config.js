d4e4980aba3f95f2a4c0bb8bb48398726b59bee5
ylxbsoftware   <ylxbsoftware@gmail.com>
2017-03-31 09:28:14 +0800

    update something

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,25 @@
 import json from 'rollup-plugin-json';
-// import resolve from 'rollup-plugin-node-resolve';
+import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
-
+import uglify from 'rollup-plugin-uglify';
+import postcss from 'rollup-plugin-postcss';
+import cssnano from 'cssnano';
 
 export default {
   entry: 'src/main.js',
   format: 'iife',
   plugins: [json(),
-    // resolve(),
+    postcss({
+      extensions: ['.css'],
+      plugins:[cssnano()]
+    }),
+    resolve(),
+    uglify(),
     babel({
       exclude: 'node_modules/**' // only transpile our source code
     })
   ],
   sourceMap: true,
   dest: 'dist/bundle.js',
-  moduleName:'bundle'
+  moduleName: 'bundle'
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import {version} from '../package.json';
+import './main.css';
+import a from './index.js';
+
+console.log($('body').length);
+document.write(
+     '<script src="http://' + (location.host || 'localhost').split(':')[0] +
+     ':35729/livereload.js?snipver=1"></script>');
 
 document.body.innerHTML = version;
 
